687317763a96632ee3e03d7c0e3af2f853b42d6e
Nathan Brenton <nbrenton@gmail.com>
2026-04-27 12:19:00 -0700

    Add database session configuration

--- app/core/config.py
+++ app/core/config.py
@@ -3,8 +3,11 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+
 class Settings:
     APP_NAME: str = os.getenv("APP_NAME", "SignalStack API")
     ENV: str = os.getenv("ENV", "dev")
+    DATABASE_URL: str = os.getenv("DATABASE_URL", "")
+
 
 settings = Settings()
