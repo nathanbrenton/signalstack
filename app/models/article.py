98c1b5cc11a86a73baa2debf05df93642697121c
Nathan Brenton <nbrenton@gmail.com>
2026-04-27 16:27:46 -0700

    Adfd created_at timestamp to articles

--- app/models/article.py
+++ app/models/article.py
@@ -1,4 +1,5 @@
-from sqlalchemy import Column, Integer, String, Text
+from sqlalchemy import Column, Integer, String, Text, DateTime
+from datetime import datetime
 
 from app.db.base import Base
 
@@ -11,4 +12,5 @@ class Article(Base):
     url = Column(String, unique=True, nullable=False, index=True)
     source = Column(String, nullable=True)
     content = Column(Text, nullable=True)
+    created_at = Column(DateTime, default=datetime.utcnow)
 
