24c8def80ef9d9f6bd49b6869be7239a4514fc6e
Nathan Brenton <nbrenton@gmail.com>
2026-04-28 16:32:09 -0700

    Add untracked files

---
+++ alembic/versions/a2a4c21f6c20_add_clean_summary_to_articles.py
@@ -0,0 +1,32 @@
+"""add clean_summary to articles
+
+Revision ID: a2a4c21f6c20
+Revises: b8ec71787004
+Create Date: 2026-04-28 15:07:38.014175
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = 'a2a4c21f6c20'
+down_revision: Union[str, Sequence[str], None] = 'b8ec71787004'
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    """Upgrade schema."""
+    op.add_column(
+        "articles",
+        sa.Column("clean_summary", sa.String(), nullable=True)
+    )
+
+
+def downgrade() -> None:
+    """Downgrade schema."""
+    op.drop_column("articles", "clean_summary")
+    
