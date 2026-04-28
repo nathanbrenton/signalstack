15a7d98fda0cf525f2af79916203d96ff943184b
Nathan Brenton <nbrenton@gmail.com>
2026-04-27 19:01:33 -0700

    Support multiple RSS feeds

--- app/ingestion/fetch_rss.py
+++ app/ingestion/fetch_rss.py
@@ -4,60 +4,66 @@ from app.models.article import Article
 import feedparser
 
 
-RSS_URL = "https://feeds.bbci.co.uk/news/world/rss.xml"
+#RSS_URL = "https://feeds.bbci.co.uk/news/world/rss.xml"
+RSS_FEEDS = [
+    "https://feeds.bbci.co.uk/news/world/rss.xml",
+    "https://www.aljazeera.com/xml/rss/all.xml",
+]
 
+
+def ingest_feed(db, rss_url):
+    feed = feedparser.parse(rss_url)
+    source_name = feed.feed.get("title")
+
+    print(f"Feed title: {source_name}")
+    print(f"Entries found: {len(feed.entries)}")
+
+    for entry in feed.entries[:5]:
+        title = entry.get("title")
+        url = entry.get("link")
+
+        if not title or not url:
+            print("Skipping invalid entry")
+            continue
+
+        published_parsed = entry.get("published_parsed")
+
+        published_at = None
+        if published_parsed:
+            published_at = datetime(*published_parsed[:6])
+
+        print(f"Published at: {published_at}")
+        print(f"Source: {source_name}")
+
+        existing_article = db.query(Article).filter(Article.url == url).first()
+
+        if existing_article:
+            print(f"Skipping duplicate: {title}")
+            continue
+
+        print(f"Inserting: {title}")
+
+        article = Article(
+            title=title,
+            url=url,
+            published_at=published_at,
+            source_name=source_name,
+        )
+
+        db.add(article)
+        db.commit()
+        db.refresh(article)
+
+        print(f"Inserted ID: {article.id}")
 
 def main():
     db = SessionLocal()
 
     try:
-        feed = feedparser.parse(RSS_URL)
-        source_name = feed.feed.get("title")
-
-        print(f"Feed title: {feed.feed.get('title')}")
-        print(f"Entries found: {len(feed.entries)}")
-
-        for entry in feed.entries[:5]: # change number here to test (e.g. 1)
-            title = entry.get("title")
-            url = entry.get("link")
-
-#            print(entry.keys())
-#            published = entry.get("published")
-#            print(f"Published: {published}")
-            published_parsed = entry.get("published_parsed")
-
-            published_at = None
-            if published_parsed:
-                published_at = datetime(*published_parsed[:6])
-
-            print(f"Published at: {published_at}")
-            print(f"Source: {source_name}")
-
-
-            if not title or not url:
-                print("Skipping invalid entry")
-                continue
-
-            existing_article = db.query(Article).filter(Article.url == url).first()
-
-            if existing_article:
-                print(f"Skipping duplicate: {title}")
-                continue
-
-            print(f"Inserting: {title}")
-
-            article = Article(
-                title=title,
-                url=url,
-                published_at=published_at,
-                source_name=source_name,
-            )
-
-            db.add(article)
-            db.commit()
-            db.refresh(article)
-
-            print(f"Inserted ID: {article.id}")
+        for rss_url in RSS_FEEDS:
+            print()
+            print(f"Processing feed: {rss_url}")
+            ingest_feed(db, rss_url)
 
     finally:
         db.close()
